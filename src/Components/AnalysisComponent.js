81e51b27f2d9d91365c8e87b1649ab476abe25ea
warrenniu <warrenniu@gmail.com>
2021-01-21 17:47:59 -0500

    Working on second pie

--- src/Components/AnalysisComponent.js
+++ src/Components/AnalysisComponent.js
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react'
-import { PieChart, Pie, Legend, Tooltip } from 'recharts';
-import { useSelector, useDispatch } from 'react-redux'
+import { PieChart, Pie, Cell, Legend, Tooltip } from 'recharts';
 import { connect } from 'react-redux'
 
 // const data01 = [
@@ -9,11 +8,11 @@ import { connect } from 'react-redux'
 //     { name: 'Group E', value: 278 }, { name: 'Group F', value: 189 },
 // ];
 
-const AnalysisComponent = (props) => {
-    // const state = useSelector(state => state.dreams)
+function AnalysisComponent(props) {
     // static jsfiddleUrl = 'https://jsfiddle.net/alidingling/k9jkog04/';
+    
 
-    function checkRating(value) {
+    const checkRating = (value) => {
         // const rating = props.dreams.map(dreamEl => dreamEl.rating === value)
         // console.log(rating)
         // return rating.length
@@ -25,28 +24,64 @@ const AnalysisComponent = (props) => {
 
         })
         return array.length
-
     }
-    const data01 = [
-        { name: 'One Star', value: checkRating(1) }, { name: 'Two Stars', value: checkRating(2) },
-        { name: 'Three Stars', value: checkRating(3) }, { name: 'Four Stars', value: checkRating(4) },
-        { name: 'Five Stars', value: checkRating(5) },
-    ];
+    
+        const data01 = [
+            { name: 'One Star', value: checkRating(1) }, { name: 'Two Stars', value: checkRating(2) },
+            { name: 'Three Stars', value: checkRating(3) }, { name: 'Four Stars', value: checkRating(4) },
+            { name: 'Five Stars', value: checkRating(5) },
+        ];
 
+        const data02 = [
+            { name: 'One Star', value: checkRating(1) }, { name: 'Two Stars', value: checkRating(2) },
+            { name: 'Three Stars', value: checkRating(3) }, { name: 'Four Stars', value: checkRating(4) },
+            { name: 'Five Stars', value: checkRating(5) },
+        ]; 
+        
+
+    const colors = ['red', 'purple', 'orange', 'blue', 'green']
+        // const { opacity } = this.state;
     return (
+        <div>
+            <h3>Dreams by Rating</h3>
         <PieChart width={400} height={400}>
-            <Pie dataKey="value" isAnimationActive={false} data={data01} cx={200} cy={200} outerRadius={80} fill="#8884d8" label />
+            <Pie dataKey="value" isAnimationActive={false} data={data01} cx={200} cy={200} outerRadius={80}>
+                {
+                    data01.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={colors[index]}/>
+                    ))
+                }
+            </Pie>
+            <Legend layout="horizontal" align="center" width={400} />
             {/* <Pie dataKey="value" data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" /> */}
             <Tooltip />
         </PieChart>
+        <div>
+            <h3>Dreams by Month</h3>
+        <PieChart width={400} height={400}>
+            <Pie dataKey="value" isAnimationActive={false} data={data02} cx={200} cy={200} outerRadius={80}>
+                {
+                    data01.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={colors[index]}/>
+                    ))
+                }
+            </Pie>
+            <Legend layout="horizontal" align="center" width={400} />
+            {/* <Pie dataKey="value" data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" /> */}
+            <Tooltip />
+        </PieChart>
+        </div>
+        </div>
     )
-}
+    }
+
 
 
 function msp(state) {
     return {
         // user: state.user,
         dreams: state.dreams,
+        journals: state.journals
     }
 }
 
