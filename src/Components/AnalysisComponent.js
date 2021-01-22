20d7d2022371486d516f98d7e5810deb9f8c3c09
warrenniu <warrenniu@gmail.com>
2021-01-21 21:56:10 -0500

    need to fix chart display

--- src/Components/AnalysisComponent.js
+++ src/Components/AnalysisComponent.js
@@ -2,20 +2,10 @@ import React from 'react'
 import { PieChart, Pie, Cell, Legend, Tooltip } from 'recharts';
 import { connect } from 'react-redux'
 
-// const data01 = [
-//     { name: 'Group A', value: this.props.dreams.length}, { name: 'Group B', value: 300 },
-//     { name: 'Group C', value: 300 }, { name: 'Group D', value: 200 },
-//     { name: 'Group E', value: 278 }, { name: 'Group F', value: 189 },
-// ];
-
 function AnalysisComponent(props) {
-    // static jsfiddleUrl = 'https://jsfiddle.net/alidingling/k9jkog04/';
 
 
     const checkRating = (value) => {
-        // const rating = props.dreams.map(dreamEl => dreamEl.rating === value)
-        // console.log(rating)
-        // return rating.length
         const array = []
         props.dreams.forEach(dreamEl => {
             if (dreamEl.rating === value) {
@@ -35,7 +25,6 @@ function AnalysisComponent(props) {
         })
         if (array.length > 0) {
             const sum = array.reduce((result, number) => result + number)
-            console.log(sum)
             return sum
         }
         else
@@ -58,9 +47,9 @@ function AnalysisComponent(props) {
     ];
 
 
-    const colors = ['red', 'purple', 'orange', 'blue', 'green', 'pink', 'silver', 'brown', 'turquoise', 'black', 'lime', 'teal' ]
+    const colors = ['red', 'purple', 'orange', 'blue', 'green', 'pink', 'silver', 'brown', 'turquoise', 'black', 'lime', 'teal']
     return (
-        <div>
+        <div className="pieChart">
             <h3>Dreams by Rating</h3>
             <PieChart width={400} height={400}>
                 <Pie dataKey="value" isAnimationActive={false} data={data01} cx={200} cy={200} outerRadius={80}>
