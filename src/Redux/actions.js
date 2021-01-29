7af65a8ce821460f98c59b3f4d61a08211afaa45
warrenniu <warrenniu@gmail.com>
2021-01-29 17:14:06 -0500

    cleaned up actions

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -1,6 +1,14 @@
-import { GET_JOURNALS, POST_JOURNAL, PATCH_JOURNAL, DELETE_JOURNAL, GET_USER, REMOVE_USER, POST_USER, POST_LOGIN, POST_DREAM, GET_DREAMS, PATCH_DREAM, DELETE_DREAM, POST_IMAGE, SEARCH } from './actionTypes'
+import { GET_USER, GET_JOURNALS, GET_DREAMS, POST_USER, POST_LOGIN, POST_JOURNAL, POST_DREAM, PATCH_JOURNAL, PATCH_DREAM, DELETE_JOURNAL, DELETE_DREAM,  REMOVE_USER, SEARCH } from './actionTypes'
 
 const BASE_URL = "http://localhost:4000"
+
+
+/************ GET ACTIONS ***************/
+export function getUser(currentUser) {
+	return function (dispatch) {
+        dispatch({ type: GET_USER, payload: currentUser })
+    }
+}
 
 export function getJournals() {
     const token = localStorage.getItem("token")
@@ -19,87 +27,24 @@ export function getJournals() {
     }
 }
 
-export function postJournal(newJournalObj) {
+export function getDreams() {
     const token = localStorage.getItem("token")
     return function (dispatch) {
-        fetch(`${BASE_URL}/api/v1/journals`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`
-            },
-            body: JSON.stringify(newJournalObj),
-        })
-            .then(response => response.json())
-            .then(journalObj => {
-                dispatch({ type: POST_JOURNAL, payload: journalObj })
-            })
-    }
-}
-
-export function patchJournal(updatedJournalObj) {
-    const token = localStorage.getItem("token")
-	return function (dispatch) {
-		fetch(`${BASE_URL}/api/v1/journals/${updatedJournalObj.id}`, {
-			method: 'PATCH',
-			headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`
-			},
-			body: JSON.stringify(updatedJournalObj),
-		})
-			.then(response => response.json())
-			.then(journalObj => {
-				dispatch({ type: PATCH_JOURNAL, payload: journalObj })
-			})
-	}
-}
-
-export function deleteJournal(journalObj) {
-    const token = localStorage.getItem("token")
-    return function (dispatch) {
-        fetch(`${BASE_URL}/api/v1/journals/${journalObj.id}`, {
-            method: 'DELETE',
+        fetch(`${BASE_URL}/api/v1/dreams`, {
+            method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
                 Authorization: `Bearer ${token}`
             }
         })
             .then(response => response.json())
-            .then(data => {
-                dispatch({ type: DELETE_JOURNAL, payload: journalObj })
+            .then(arrayOfDreams => {
+                dispatch({ type: GET_DREAMS, payload: arrayOfDreams })
             })
-
     }
 }
 
-// export function getUser() {
-//     return function (dispatch) {
-//         fetch(`${BASE_URL}/api/v1/users/1`, {
-//             method: 'GET',
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             }
-//         })
-//             .then(response => response.json())
-//             .then(userObj => {
-//                 dispatch({ type: GET_USER, payload: userObj })
-//             })
-//     }
-// }
-
-export function getUser(currentUser) {
-	return function (dispatch) {
-        dispatch({ type: GET_USER, payload: currentUser })
-        
-    }
-}
-
-export function removeUser() {
-	return function (dispatch) {
-		dispatch({ type: REMOVE_USER })
-	}
-}
+/************ POST ACTIONS ***************/
 
 export function postUser(newUser) {
     return function (dispatch) {
@@ -136,19 +81,20 @@ export function postLogin(userInfo) {
     }
 }
 
-export function getDreams() {
+export function postJournal(newJournalObj) {
     const token = localStorage.getItem("token")
     return function (dispatch) {
-        fetch(`${BASE_URL}/api/v1/dreams`, {
-            method: 'GET',
+        fetch(`${BASE_URL}/api/v1/journals`, {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 Authorization: `Bearer ${token}`
-            }
+            },
+            body: JSON.stringify(newJournalObj),
         })
             .then(response => response.json())
-            .then(arrayOfDreams => {
-                dispatch({ type: GET_DREAMS, payload: arrayOfDreams })
+            .then(journalObj => {
+                dispatch({ type: POST_JOURNAL, payload: journalObj })
             })
     }
 }
@@ -168,8 +114,27 @@ export function postDream(newDreamObj) {
             .then(dreamObj => {
                 dispatch({ type: POST_DREAM, payload: dreamObj })
             })
-    }
-    
+    } 
+}
+
+/************ PATCH ACTIONS ***************/
+
+export function patchJournal(updatedJournalObj) {
+    const token = localStorage.getItem("token")
+	return function (dispatch) {
+		fetch(`${BASE_URL}/api/v1/journals/${updatedJournalObj.id}`, {
+			method: 'PATCH',
+			headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+			},
+			body: JSON.stringify(updatedJournalObj),
+		})
+			.then(response => response.json())
+			.then(journalObj => {
+				dispatch({ type: PATCH_JOURNAL, payload: journalObj })
+			})
+	}
 }
 
 export function patchDream(updatedDreamObj) {
@@ -190,6 +155,8 @@ export function patchDream(updatedDreamObj) {
 	}
 }
 
+/************ DELETE ACTIONS ***************/
+
 export function deleteDream(dreamObj) {
     const token = localStorage.getItem("token")
     return function (dispatch) {
@@ -208,8 +175,34 @@ export function deleteDream(dreamObj) {
     }
 }
 
+export function deleteJournal(journalObj) {
+    const token = localStorage.getItem("token")
+    return function (dispatch) {
+        fetch(`${BASE_URL}/api/v1/journals/${journalObj.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                dispatch({ type: DELETE_JOURNAL, payload: journalObj })
+            })
+    }
+}
+
+export function removeUser() {
+	return function (dispatch) {
+		dispatch({ type: REMOVE_USER })
+	}
+}
+
+/************ SEARCH ACTION ***************/
+
 export function search(value) {
     return function (dispatch) {
         dispatch({type: SEARCH, payload: value})
     }
 }
+
